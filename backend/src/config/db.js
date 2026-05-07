8db972e6195eb0b4baf7cb55ed67c329c072a8d3
Nihal Shetty <nihalshetty.cs22@sahyadri.edu.in>
2026-05-07 11:33:33 +0530

    fix: detect if URI already has db name to avoid /aitasks.tasks error

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,8 +4,12 @@ import { env } from "./env.js";
 export async function connectDb() {
   mongoose.set("strictQuery", true);
   let uri = env.mongoUri;
-  uri = uri.split("?")[0] + "/aitasks?retryWrites=true&w=majority";
-  console.log("Connecting to MongoDB:", uri.split("@")[0] + "@...");
+  const hasDb = uri.match(/\/[a-zA-Z0-9_]+\?/);
+  if (!hasDb) {
+    const base = uri.split("?")[0];
+    uri = base + "/aitasks?retryWrites=true&w=majority";
+  }
+  console.log("MongoDB connecting to:", uri.replace(/\/\/[^@]+@/, "//user:pass@"));
   await mongoose.connect(uri, {
     autoCreate: true
   });
